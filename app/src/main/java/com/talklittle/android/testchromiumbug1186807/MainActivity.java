e50c03e83989b9fa3d745d1a784780e06d821467
Andrew Shu <talklittle@gmail.com>
2021-03-11 23:33:38 -0800

    Add WebView and reproduce bug

--- app/src/main/java/com/talklittle/android/testchromiumbug1186807/MainActivity.java
+++ app/src/main/java/com/talklittle/android/testchromiumbug1186807/MainActivity.java
@@ -3,6 +3,7 @@ package com.talklittle.android.testchromiumbug1186807;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.webkit.WebView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        WebView webView = findViewById(R.id.webview);
+
+        webView.stopLoading();
+        webView.loadUrl("https://en.m.wikipedia.org/wiki/Main_Page");
     }
 }
