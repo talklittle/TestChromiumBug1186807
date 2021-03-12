81263a1f92742a6562009d6f22e1500569b30fc7
Andrew Shu <talklittle@gmail.com>
2021-03-11 23:29:52 -0800

    Initial commit
    Generated by Android Studio 4.1.2

---
+++ app/src/main/java/com/talklittle/android/testchromiumbug1186807/MainActivity.java
@@ -0,0 +1,14 @@
+package com.talklittle.android.testchromiumbug1186807;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
